c95f793e843c64a312969d79b7802ec3ea690284
Luis Pacheco <lpachecoby@gmail.com>
2020-07-21 11:59:31 -0500

    archivos

--- portafolio.php
+++ portafolio.php
@@ -208,7 +208,7 @@
 </div>
 
 <!-- Modal 5-->
-<div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="close-modal" data-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal"/></div>
