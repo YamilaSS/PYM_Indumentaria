36a7dc7484274a17945a433b8176e41eca74f92c
Luis Pacheco <lpachecoby@gmail.com>
2020-07-21 11:23:55 -0500

    archivos

--- portafolio.php
+++ portafolio.php
@@ -38,16 +38,12 @@
                         <div class="modal-body">
                             <!-- Project Details Go Here-->
                             <h2 class="text-uppercase">Push-Up Rosa y Azul</h2>
-                            <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                            <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/01-full.jpg" alt=""/>
-                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt
-                                repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae,
-                                nostrum, reiciendis facere nemo!</p>
+                            <p class="item-intro text-muted">Seguridad y Confort.</p>
+                            <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/Push%20Up%20Rosa%20y%20azul.png" alt=""/>
+                            <p>Envianos un mensaje al número: 970523305</p>
                             <ul class="list-inline">
-                                <li>Date: January 2020</li>
-                                <li>Client: Threads</li>
-                                <li>Category: Illustration</li>
+                                <li>Tallas 34 - 40</li>
+                                <li>Precio Especial: S/.30</li>
                             </ul>
                             <button class="btn btn-primary" data-dismiss="modal" type="button">
                                 <i class="fas fa-times mr-1"></i>
