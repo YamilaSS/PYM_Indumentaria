ecbda843227546de02d8740206087b85a0e7798a
Luis Pacheco <lpachecoby@gmail.com>
2020-07-21 09:38:55 -0500

    archivos

--- index.php
+++ index.php
@@ -41,45 +41,47 @@
     <div class="container">
         <div class="masthead-subheading">Bienvenido</div>
         <div class="masthead-heading text-uppercase">Nos encanta tenerte aquí</div>
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Catálogo</a>
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#portfolio">Catálogo</a>
     </div>
 </header>
 <!-- Services-->
 <section class="page-section" id="services">
     <div class="container">
         <div class="text-center">
-            <h2 class="section-heading text-uppercase">Services</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h2 class="section-heading text-uppercase">Servicios</h2>
+            <h3 class="section-subheading text-muted">Venta de ropa con estilo para ti.</h3>
         </div>
         <div class="row text-center">
             <div class="col-md-4">
                         <span class="fa-stack fa-4x">
                             <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
                         </span>
-                <h4 class="my-3">E-Commerce</h4>
-                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <h4 class="my-3">Consulta</h4>
+                <p class="text-muted">Nosotros te facilitamos toda la información del producto que te interesa.</p>
             </div>
             <div class="col-md-4">
                         <span class="fa-stack fa-4x">
                             <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
                         </span>
-                <h4 class="my-3">Responsive Design</h4>
-                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <h4 class="my-3">Compra</h4>
+                <p class="text-muted">Haz tu pedido al mejor precio del mercado.</p>
             </div>
             <div class="col-md-4">
                         <span class="fa-stack fa-4x">
                             <i class="fas fa-circle fa-stack-2x text-primary"></i>
                             <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
                         </span>
-                <h4 class="my-3">Web Security</h4>
-                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <h4 class="my-3">Pide</h4>
+                <p class="text-muted">Si no encontraste lo que buscabas solicitalo y nosotros lo encontraremos por ti.</p>
             </div>
         </div>
     </div>
 </section>
 <!-- Portfolio Grid-->
+<?php include 'portafolio.php'; ?>
+<!--
 <section class="page-section bg-light" id="portfolio">
     <div class="container">
         <div class="text-center">
@@ -174,6 +176,7 @@
         </div>
     </div>
 </section>
+-->
 <!-- About-->
 <section class="page-section" id="about">
     <div class="container">
