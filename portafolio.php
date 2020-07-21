1130b1868cc247ad0e987ce26b12b7125ba389ab
Luis Pacheco <lpachecoby@gmail.com>
2020-07-21 11:38:08 -0500

    archivos

--- portafolio.php
+++ portafolio.php
@@ -1,4 +1,4 @@
-<!-- Portfolio Grid-->
+<!-- Productos 1-->
 <section class="page-section bg-light" id="portfolio">
     <div class="container">
         <div class="text-center">
@@ -25,8 +25,6 @@
     </div>
 </section>
 
-
-<!-- Portfolio Modals-->
 <!-- Modal 1-->
 <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog">
@@ -39,7 +37,8 @@
                             <!-- Project Details Go Here-->
                             <h2 class="text-uppercase">Push-Up Rosa y Azul</h2>
                             <p class="item-intro text-muted">Seguridad y Confort.</p>
-                            <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/Push%20Up%20Rosa%20y%20azul.png" alt=""/>
+                            <img class="img-fluid d-block mx-auto"
+                                 src="assets/img/portfolio/Push%20Up%20Rosa%20y%20azul.png" alt=""/>
                             <p>Envianos un mensaje al número: 970523305</p>
                             <ul class="list-inline">
                                 <li>Tallas 34 - 40</li>
@@ -47,7 +46,65 @@
                             </ul>
                             <button class="btn btn-primary" data-dismiss="modal" type="button">
                                 <i class="fas fa-times mr-1"></i>
-                                Close Project
+                                Cerrar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Productos 2-->
+<section class="page-section bg-light" id="portfolio">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Productos</h2>
+            <h3 class="section-subheading text-muted"></h3>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/Colores%20enteros%20Push%20up%20.png" alt=""/>
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">Push-Up Colores enteros</div>
+                        <div class="portfolio-caption-subheading text-muted"></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<!-- Modal 2-->
+<div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal"/></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="modal-body">
+                            <!-- Project Details Go Here-->
+                            <h2 class="text-uppercase">Push-Up Colores enteros</h2>
+                            <p class="item-intro text-muted">Seguridad y Confort.</p>
+                            <img class="img-fluid d-block mx-auto"
+                                 src="assets/img/portfolio/Colores%20enteros%20Push%20up%20.png" alt=""/>
+                            <p>Envianos un mensaje al número: 970523305</p>
+                            <ul class="list-inline">
+                                <li>Tallas 34 - 40</li>
+                                <li>Precio Especial: S/.30</li>
+                            </ul>
+                            <button class="btn btn-primary" data-dismiss="modal" type="button">
+                                <i class="fas fa-times mr-1"></i>
+                                Cerrar
                             </button>
                         </div>
                     </div>
