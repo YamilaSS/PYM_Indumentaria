066da9f684a3cb3b2819b1ceb2265554006ac303
YamilaSS <perlayamilamelanie@gmail.com>
2020-07-22 00:58:23 -0500

    Yamila
    He agregado una nueva imagen a los productos

--- portafolio.php
+++ portafolio.php
@@ -76,6 +76,20 @@
                     </div>
                 </div>
             </div>
+			 <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/nueva.nuevita.jpg" alt=""/>
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">Instagram</div>
+                        <div class="portfolio-caption-subheading text-muted"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
@@ -128,7 +142,7 @@
                                  src="assets/img/portfolio/Colores%20enteros%20Push%20up%20.png" alt=""/>
                             <p>Envianos un mensaje al número: 970523305</p>
                             <ul class="list-inline">
-                                <li>Tallas 34 - 40</li>
+                                <li>Tallas 34 - 38</li>
                                 <li>Precio Especial: S/.30</li>
                             </ul>
                             <button class="btn btn-primary" data-dismiss="modal" type="button">
@@ -222,6 +236,36 @@
                             <img class="img-fluid d-block mx-auto"
                                  src="assets/img/portfolio/Strapless%20rosa%20y%20blanco.jfif" alt=""/>
                             <p>Envianos un mensaje al número: 970523305</p>
+                            <ul class="list-inline">
+                                <li>Tallas 38 - 44</li>
+                                <li>Precio Especial: S/.35</li>
+                            </ul>
+                            <button class="btn btn-primary" data-dismiss="modal" type="button">
+                                <i class="fas fa-times mr-1"></i>
+                                Cerrar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal 6-->
+<div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal"/></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="modal-body">
+                            <!-- Project Details Go Here-->
+                            <h2 class="text-uppercase">Instagram</h2>
+                            <p class="item-intro text-muted">Seguridad y Confort.</p>
+                            <img class="img-fluid d-block mx-auto"
+                                 src="assets/img/portfolio/nueva.nuevita.jpg" alt=""/>
+                            <p>Envianos un mensaje al número: 970523305/999614206</p>
                             <ul class="list-inline">
                                 <li>Tallas 38 - 44</li>
                                 <li>Precio Especial: S/.35</li>
